ceadc22986ed9429f7ab7cdcc25703c9afba5e32
Martin Donadieu <martindonadieu@gmail.com>
2026-05-12 21:09:16 +0200

    fix: align ios package name with capacitor

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapgoNativeNavigation",
+    name: "CapgoCapacitorNativeNavigation",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "CapgoNativeNavigation",
+            name: "CapgoCapacitorNativeNavigation",
             targets: ["NativeNavigationPlugin"])
     ],
     dependencies: [
