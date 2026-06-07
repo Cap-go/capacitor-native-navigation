7d86a959ea5269b42bfa9a9a3658e0d25b0ffb75
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:41 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,22 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.native.navigation.example",
-  "appName": "Native Navigation Example",
-  "webDir": "dist",
-  "plugins": {
-    "CapacitorUpdater": {
-      "appId": "app.capgo.native.navigation.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
+  appId: 'app.capgo.native.navigation.example',
+  appName: 'Native Navigation Example',
+  webDir: 'dist',
+  plugins: {
+    CapacitorUpdater: {
+      appId: 'app.capgo.native.navigation.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
   },
-  "android": {
-    "webContentsDebuggingEnabled": true
-  }
+  android: {
+    webContentsDebuggingEnabled: true,
+  },
 };
 
 export default config;
